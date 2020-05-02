acca04e1e66e8e4957d0bfe50f8f8d76fa0bf5fa
oppilate <oppilate@tutanota.com>
2020-05-02 16:40:19 +0800

    Switch database type to postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,9 @@ import { FulltextExtractModule } from './fulltext-extract/fulltext-extract.modul
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres',
       host: 'localhost',
-      port: 3306,
+      port: 5432,
       username: 'root',
       password: 'root',
       database: 'test',
