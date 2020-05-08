4ffc2a8978c547c9ed6204000b6c8a7272e48270
oppilate <oppilate@tutanota.com>
2020-05-08 14:46:41 +0800

    Rearchitect crawler by Queue
    Signed-off-by: oppilate <oppilate@tutanota.com>

--- src/rss-crawler/rss-crawler.module.ts
+++ src/rss-crawler/rss-crawler.module.ts
@@ -1,3 +1,4 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { AbstractGenerationModule } from '../abstract-generation/abstract-generation.module';
 import { FulltextExtrationModule } from '../fulltext-extration/fulltext-extration.module';
@@ -5,6 +6,16 @@ import { RSSCrawlerService } from './rss-crawler.service';
 
 @Module({
   providers: [RSSCrawlerService],
-  imports: [FulltextExtrationModule, AbstractGenerationModule],
+  imports: [
+    FulltextExtrationModule,
+    AbstractGenerationModule,
+    BullModule.registerQueue({
+      name: 'crawler',
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+  ],
 })
 export class RSSCrawlerModule {}
