acf9e2b0d5faf5c647301d4d4421c6c1a66eff0b
oppilate <oppilate@tutanota.com>
2020-05-09 23:44:41 +0800

    Add commnets to classes as much as possibleSigned-off-by: oppilate <oppilate@tutanota.com>
    Signed-off-by: oppilate <oppilate@tutanota.com>

--- src/dynamic-page-archiving/dynamic-page-archiving.service.ts
+++ src/dynamic-page-archiving/dynamic-page-archiving.service.ts
@@ -2,7 +2,9 @@ import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
 import { Job, Queue } from 'bull';
 import { Url } from 'url';
-
+/**
+ * Archiving job producer. Should revoke this in other modules.
+ */
 @Injectable()
 export class DynamicPageArchivingService {
   constructor(
@@ -10,6 +12,10 @@ export class DynamicPageArchivingService {
     private dynamicPageArchivingQueue: Queue
   ) {}
 
+  /**
+   * Loads and snapshots a dynamic loading page.
+   * @param url url of the page to snapshot
+   */
   async archive(url: Url): Promise<string> {
     // Archive dynamic page
     let job = (await this.dynamicPageArchivingQueue.add(url)) as Job<string>;
--- src/site/site.service.ts
+++ src/site/site.service.ts
@@ -4,27 +4,50 @@ import { Repository } from 'typeorm';
 import { CreateSiteDto } from './dto/create-site.dto';
 import { FindOneSiteDto } from './dto/find-one-site.dto';
 import { Site } from './site.entity';
+/**
+ * Manages sites. Site should be completely unque.
+ */
 @Injectable()
 export class SiteService {
   constructor(
     @InjectRepository(Site) private siteRepository: Repository<Site>
   ) {}
+  /**
+   * Create a site record. Will fail if it exists
+   * @param candidateSite known info of the site
+   */
   async create(candidateSite: CreateSiteDto): Promise<Site> {
     const newSite = this.siteRepository.create(candidateSite);
     await this.siteRepository.save(newSite);
     return newSite;
   }
 
+  /**
+   * Find sites with the given criteria. Since every site is unique, it will return an array of one site.
+   * @param findOneSiteDto critera of the site to find
+   */
   findAll(findOneSiteDto: FindOneSiteDto): Promise<Site[]> {
     return this.siteRepository.find(findOneSiteDto);
   }
 
+  /**
+   * Find a site with the given id
+   * @param id internal ID of the site
+   */
   findOne(id: number): Promise<Site>;
+  /**
+   * Find a site with the given conditioin
+   * @param conditions critera of the site to find
+   */
   findOne(conditions: FindOneSiteDto): Promise<Site>;
   findOne(conditions: any) {
     return this.siteRepository.findOneOrFail(conditions);
   }
 
+  /**
+   * Delete a site with the given id
+   * @param id internal ID of the sit
+   */
   async deleteOne(id: number): Promise<{ deleted: boolean; message?: string }> {
     try {
       await this.siteRepository.delete({ id });
@@ -34,6 +57,9 @@ export class SiteService {
     }
   }
 
+  /**
+   * Returns all sites in database
+   */
   async getAll(): Promise<Site[]> {
     return this.siteRepository.find();
   }
