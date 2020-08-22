6976562333308eb82decfdfe10f35d28620d23b5
oppilate <oppilate@tutanota.com>
2020-08-23 01:02:30 +0800

    test: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  let app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  const configService = app.get(ConfigService);
+	let app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(new ValidationPipe());
+	const configService = app.get(ConfigService);
 
-  // Set log level
-  const env = configService.get<string>('NODE_ENV');
-  const logLevels: LogLevel[] =
-    env === 'production'
-      ? ['error', 'warn']
-      : ['log', 'error', 'warn', 'debug', 'verbose'];
+	// Set log level
+	const env = configService.get<string>('NODE_ENV');
+	const logLevels: LogLevel[] =
+		env === 'production' ? [ 'error', 'warn' ] : [ 'log', 'error', 'warn', 'debug', 'verbose' ];
 
-  app = await NestFactory.create(AppModule, {
-    logger: logLevels,
-  });
+	app = await NestFactory.create(AppModule, {
+		logger: logLevels
+	});
 
-  // Set listening port
-  await app.listen(configService.get<number>('PORT'));
+	// Allow CORS in development envorinment
+	if (env === 'development') app.enableCors();
+
+	// Set listening port
+	await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
