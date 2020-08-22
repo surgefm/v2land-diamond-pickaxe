84c308b9c94f8c792b18454fca57d2401f111372
oppilate <oppilate@tutanota.com>
2020-08-23 01:01:58 +0800

    fix: mixed style of relative path

--- scripts/add_site_from_yaml.ts
+++ scripts/add_site_from_yaml.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import fs from 'fs';
 import yaml from 'js-yaml';
-import { CreateSiteDto } from 'src/site/dto/create-site.dto';
+import { CreateSiteDto } from '../src/site/dto/create-site.dto';
 
 // https://example.com
 const instance = process.env.INSTANCE;
@@ -9,15 +9,13 @@ const endpoint = '/site';
 const url = instance + endpoint;
 
 try {
-  const site_yaml_file = process.argv[2];
-  const sites = yaml.safeLoad(
-    fs.readFileSync(site_yaml_file, 'utf8')
-  ) as CreateSiteDto[];
+	const site_yaml_file = process.argv[2];
+	const sites = yaml.safeLoad(fs.readFileSync(site_yaml_file, 'utf8')) as CreateSiteDto[];
 
-  sites.forEach(async site => {
-    console.log(site);
-    await axios.post(url, site);
-  });
+	sites.forEach(async (site) => {
+		console.log(site);
+		await axios.post(url, site);
+	});
 } catch (e) {
-  console.log(e);
+	console.log(e);
 }
--- src/article/article.service.spec.ts
+++ src/article/article.service.spec.ts
@@ -1,163 +1,153 @@
 import { getModelToken } from '@nestjs/sequelize';
 import { Test, TestingModule } from '@nestjs/testing';
-import { Site } from 'src/site/site.model';
+import { Site } from '../site/site.model';
 import { Article } from './article.model';
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { FindArticleDto } from './dto/find-article.dto';
 
 const testSite = new Site({
-  id: 1,
-  name: 'Apple',
-  rssUrls: ['https://rsshub.app/apple/exchange_repair/zh-cn'],
-  shouldParseFulltext: true,
-  dynamicLoading: true,
-  articles: [],
+	id: 1,
+	name: 'Apple',
+	rssUrls: [ 'https://rsshub.app/apple/exchange_repair/zh-cn' ],
+	shouldParseFulltext: true,
+	dynamicLoading: true,
+	articles: []
 });
 const testCreateArticleDto = {
-  name: 'Apple 更换和维修扩展计划',
-  url: 'https://rsshub.app/apple/exchange_repair/zh-cn',
-  status: 'pending',
-  site: testSite,
-  author: 'authorname',
+	name: 'Apple 更换和维修扩展计划',
+	url: 'https://rsshub.app/apple/exchange_repair/zh-cn',
+	status: 'pending',
+	site: testSite,
+	author: 'authorname'
 } as CreateArticleDto;
 const testArticleId = 123;
 const testArticle1 = { id: testArticleId, ...testCreateArticleDto } as Article;
 const testFindArticleDto: FindArticleDto = testCreateArticleDto;
 const articleArray = [
-  testArticle1,
-  {
-    id: 1,
-    name: 'Apple 更换和维修扩展计划',
-    url: 'https://rsshub.app/apple/exchange_repair/zh-cn',
-    status: 'pending',
-    site: {
-      id: 1,
-      name: 'Apple',
-      rssUrls: ['https://rsshub.app/apple/exchange_repair/zh-cn'],
-      shouldParseFulltext: true,
-      dynamicLoading: true,
-      articles: [],
-    },
-    author: 'authorname',
-  },
-  {
-    id: 2,
-    name: 'Apple 更换和维修扩展计划',
-    url: 'https://rsshub.app/apple/exchange_repair/zh-cn',
-    status: 'pending',
-    site: {
-      id: 1,
-      name: 'Apple',
-      rssUrls: ['https://rsshub.app/apple/exchange_repair/zh-cn'],
-      shouldParseFulltext: true,
-      dynamicLoading: true,
-      articles: [],
-    },
-    author: 'authorname',
-  },
+	testArticle1,
+	{
+		id: 1,
+		name: 'Apple 更换和维修扩展计划',
+		url: 'https://rsshub.app/apple/exchange_repair/zh-cn',
+		status: 'pending',
+		site: {
+			id: 1,
+			name: 'Apple',
+			rssUrls: [ 'https://rsshub.app/apple/exchange_repair/zh-cn' ],
+			shouldParseFulltext: true,
+			dynamicLoading: true,
+			articles: []
+		},
+		author: 'authorname'
+	},
+	{
+		id: 2,
+		name: 'Apple 更换和维修扩展计划',
+		url: 'https://rsshub.app/apple/exchange_repair/zh-cn',
+		status: 'pending',
+		site: {
+			id: 1,
+			name: 'Apple',
+			rssUrls: [ 'https://rsshub.app/apple/exchange_repair/zh-cn' ],
+			shouldParseFulltext: true,
+			dynamicLoading: true,
+			articles: []
+		},
+		author: 'authorname'
+	}
 ] as Article[];
 
 describe('ArticleService', () => {
-  let service: ArticleService;
-  const mockModel = {
-    create: jest.fn().mockReturnValue(testArticle1),
-    save: jest.fn().mockReturnValue(testArticle1),
-    find: jest
-      .fn()
-      .mockImplementation((conditions?: FindArticleDto): Article[] => {
-        if (conditions == undefined) {
-          return articleArray;
-        } else {
-          return [articleArray[0]];
-        }
-      }),
-    findOneOrFail: jest.fn().mockResolvedValue(testArticle1),
-    delete: jest.fn().mockResolvedValue(true),
-  };
+	let service: ArticleService;
+	const mockModel = {
+		create: jest.fn().mockReturnValue(testArticle1),
+		save: jest.fn().mockReturnValue(testArticle1),
+		find: jest.fn().mockImplementation((conditions?: FindArticleDto): Article[] => {
+			if (conditions == undefined) {
+				return articleArray;
+			} else {
+				return [ articleArray[0] ];
+			}
+		}),
+		findOneOrFail: jest.fn().mockResolvedValue(testArticle1),
+		delete: jest.fn().mockResolvedValue(true)
+	};
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        ArticleService,
-        {
-          provide: getModelToken(Article),
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				ArticleService,
+				{
+					provide: getModelToken(Article),
 
-          // define all the methods that you use from the siteRepo
-          // give proper return values as expected or mock implementations, your choice
-          useValue: mockModel,
-        },
-      ],
-    }).compile();
+					// define all the methods that you use from the siteRepo
+					// give proper return values as expected or mock implementations, your choice
+					useValue: mockModel
+				}
+			]
+		}).compile();
 
-    service = module.get<ArticleService>(ArticleService);
-  });
+		service = module.get<ArticleService>(ArticleService);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
 
-  // create
-  describe('create', () => {
-    it('should insert a site', async () => {
-      expect(service.create(testCreateArticleDto)).resolves.toEqual(
-        testArticle1
-      );
-      expect(mockModel.find).toBeCalledTimes(1);
-    });
-  });
+	// create
+	describe('create', () => {
+		it('should insert a site', async () => {
+			expect(service.create(testCreateArticleDto)).resolves.toEqual(testArticle1);
+			expect(mockModel.find).toBeCalledTimes(1);
+		});
+	});
 
-  // findAll
-  describe('findAll', () => {
-    it('should get an array of a single article', () => {
-      const repoSpy = jest.spyOn(mockModel, 'find');
-      expect(service.findAll(testFindArticleDto)).resolves.toEqual([
-        testArticle1,
-      ]);
-      expect(repoSpy).toBeCalledWith(testFindArticleDto);
-    });
-  });
+	// findAll
+	describe('findAll', () => {
+		it('should get an array of a single article', () => {
+			const repoSpy = jest.spyOn(mockModel, 'find');
+			expect(service.findAll(testFindArticleDto)).resolves.toEqual([ testArticle1 ]);
+			expect(repoSpy).toBeCalledWith(testFindArticleDto);
+		});
+	});
 
-  // findOne
-  describe('findOne by id', () => {
-    it('should get a single article', () => {
-      const repoSpy = jest.spyOn(mockModel, 'findOneOrFail');
-      expect(service.findOne(testArticleId)).resolves.toEqual(testArticle1);
-      expect(repoSpy).toBeCalledWith(testArticleId);
-    });
-  });
-  describe('findOne by DTO', () => {
-    it('should get a single article', () => {
-      const repoSpy = jest.spyOn(mockModel, 'findOneOrFail');
-      expect(service.findOne(testFindArticleDto)).resolves.toEqual(
-        testArticle1
-      );
-      expect(repoSpy).toBeCalledWith(testFindArticleDto);
-    });
-  });
+	// findOne
+	describe('findOne by id', () => {
+		it('should get a single article', () => {
+			const repoSpy = jest.spyOn(mockModel, 'findOneOrFail');
+			expect(service.findOne(testArticleId)).resolves.toEqual(testArticle1);
+			expect(repoSpy).toBeCalledWith(testArticleId);
+		});
+	});
+	describe('findOne by DTO', () => {
+		it('should get a single article', () => {
+			const repoSpy = jest.spyOn(mockModel, 'findOneOrFail');
+			expect(service.findOne(testFindArticleDto)).resolves.toEqual(testArticle1);
+			expect(repoSpy).toBeCalledWith(testFindArticleDto);
+		});
+	});
 
-  // delete
-  describe('deleteOne', () => {
-    it('should return {deleted: true}', () => {
-      expect(service.deleteOne(1)).resolves.toEqual({ deleted: true });
-    });
-    it('should return {deleted: false, message: err.message}', () => {
-      const repoSpy = jest
-        .spyOn(mockModel, 'delete')
-        .mockRejectedValueOnce(new Error('Bad Delete Method.'));
-      expect(service.deleteOne(4)).resolves.toEqual({
-        deleted: false,
-        message: 'Bad Delete Method.',
-      });
-      expect(repoSpy).toBeCalledWith({ id: 4 });
-      expect(repoSpy).toBeCalledTimes(1);
-    });
-  });
+	// delete
+	describe('deleteOne', () => {
+		it('should return {deleted: true}', () => {
+			expect(service.deleteOne(1)).resolves.toEqual({ deleted: true });
+		});
+		it('should return {deleted: false, message: err.message}', () => {
+			const repoSpy = jest.spyOn(mockModel, 'delete').mockRejectedValueOnce(new Error('Bad Delete Method.'));
+			expect(service.deleteOne(4)).resolves.toEqual({
+				deleted: false,
+				message: 'Bad Delete Method.'
+			});
+			expect(repoSpy).toBeCalledWith({ id: 4 });
+			expect(repoSpy).toBeCalledTimes(1);
+		});
+	});
 
-  describe('getAll', () => {
-    it('should return an array of articles', async () => {
-      const articles = await service.getAll();
-      expect(articles).toEqual(articleArray);
-    });
-  });
+	describe('getAll', () => {
+		it('should return an array of articles', async () => {
+			const articles = await service.getAll();
+			expect(articles).toEqual(articleArray);
+		});
+	});
 });
