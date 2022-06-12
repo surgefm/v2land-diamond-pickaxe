4e9316fd6a32af7a1e9c4da135ad91270d0575ac
oppilate <oppilate@protonmail.com>
2022-06-12 23:28:26 +0800

    migrate HttpModule to Axios

--- src/follow-redirect/follow-redirect.module.ts
+++ src/follow-redirect/follow-redirect.module.ts
@@ -1,9 +1,10 @@
-import { HttpModule, Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { Module } from '@nestjs/common';
 import { FollowRedirectService } from './follow-redirect.service';
 
 @Module({
-  imports: [HttpModule],
-  providers: [FollowRedirectService],
-  exports: [FollowRedirectService],
+	imports: [HttpModule],
+	providers: [FollowRedirectService],
+	exports: [FollowRedirectService],
 })
 export class FollowRedirectModule {}
